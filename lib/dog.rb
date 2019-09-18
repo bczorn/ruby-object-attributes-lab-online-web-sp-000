cc113257e60093059bbfc071e2cfe202982822c3
Becky Zorn <bczorn@gmail.com>
2019-09-18 03:55:25 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  def name=(dog_name)
+    @name = dog_name
+  end
+  def name
+    @name
+  end
+  def breeds=(dog_breed)
+    @breed = dog_breed
+  end
+  def breed
+    @breed
+  end
+end
